dcbe685964cc64ed2a9f7d1971b4143bfb3b5af0
Lee-Jinyong <yjy1480@gmail.com>
2024-06-17 13:43:32 +0900

    변수와 상수

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,39 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+/* --------------------------------- 실습 과제 1 -------------------------------- */
+let admin;
+let name;
+
+name = 'john';
+admin = name;
+
+console.log(admin);
+
+/* --------------------------------- 실습 과제 2 --------------------------------- */
+const LIVING_PLANET_NAME = 'Earth';
+const currentOnlineUserName = 'user';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuantity;
+// - 구매 제품 가격의 총 합
+let totalProductsPrice;
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+// - 구매 결제 여부
+let isPayment;
+// - 구매 결제 내역
+let paymentList;
+// - 브랜드 접두사
+const BRAND_PREFIX = 'Nike';
+// - 오늘의 운세
+let fortuneToday = 'good';
+// - 상품 정보
+const productInformation = 'product';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
