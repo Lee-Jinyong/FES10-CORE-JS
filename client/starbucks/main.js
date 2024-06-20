b056b8a2c3f5a2b5cbd5e2720c632f4a6ff22bca
Lee-Jinyong <yjy1480@gmail.com>
2024-06-20 10:44:33 +0900

    토이 프로젝트 : 스타벅스 메뉴 이벤트 - GSAP 활용

--- client/starbucks/main.js
+++ client/starbucks/main.js
@@ -1,25 +1,12 @@
+/* global gsap */
+
 const aList = document.querySelectorAll('nav a');
-const depthList = document.querySelectorAll('.depth');
-const header = document.querySelector('#header');
-
-const h = t => t.style.height = 0
-
-// 매개 변수가 한 개면
-// depthList.forEach((item) => {
-//   console.log(item)
-// })
-// 아래처럼 쓸 수 있다
-// depthList.forEach(console.log);
 
 aList.forEach((a) => {
-  a.addEventListener('mouseenter', () => {
-    const target = a.lastElementChild;
+  const target = a.lastElementChild;
 
-    // 모든 depth의 높이를 0
-    depthList.forEach(h);
+  const tl = gsap.timeline({paused:true}).to(target, {height:100, ease:"power3.inOut"});
 
-    target.style.height = '100px';
-  })
-})
-
-header.addEventListener('mouseleave', () => depthList.forEach(h));
+  a.addEventListener('mouseenter', () => tl.play())
+  a.addEventListener('mouseleave', () => tl.reverse())
+});
