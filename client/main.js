577c3704c89a3067b315159635b06e6ac81cc60a
Lee-Jinyong <yjy1480@gmail.com>
2024-06-24 13:50:15 +0900

    토이 프로젝트 : 계산기
    - 결과 삭제
    - 이벤트 하나로 제어하기

--- client/main.js
+++ client/main.js
@@ -4,12 +4,18 @@
 //    - input.value 값을 가져온다.
 
 // 2. 숫자 더하기
-//     - 숫자 형변환
+//    - 숫자 형변환
 
+// 3. result 내용 비우기
+//    - clearContents
+
+
+/* global clearContents */
 
 const first = getNode('#firstNumber');
-const second = getNode('#secondNumber')
-const result = getNode('.result')
+const second = getNode('#secondNumber');
+const result = getNode('.result');
+const clear = getNode('#clear');
 
 function handleInput() {
   const firstValue = +first.value;
@@ -18,12 +24,44 @@ function handleInput() {
   // const secondValue = Number(second.value);
   const total = firstValue + secondValue;
 
-  console.log(total);
-
-  result.textContent = '';
+  clearContents(result);
 
   insertLast(result, total);
 }
 
+function handleClear(e) {
+  e.preventDefault;
+  clearContents(first);
+  clearContents(second);
+  result.textContent = '-';
+}
+
 first.addEventListener('input', handleInput);
 second.addEventListener('input', handleInput);
+clear.addEventListener('click', handleClear);
+
+
+
+function phase2() {
+  const calculator = getNode('.calculator');
+  const result = getNode('.result');
+  const clear = getNode('#clear');
+  const numberInputs = [...document.querySelectorAll('input:not(#clear)')]
+
+  function handleInput() {
+
+    const total = numberInputs.reduce((acc,cur) => acc + Number(cur.value), 0)
+    
+    clearContents(result);
+    insertLast(result,total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+    numberInputs.forEach(clearContents);
+    result.textContent = '-';
+  }
+
+  calculator.addEventListener('input',handleInput);
+  clear.addEventListener('click',handleClear);
+}
