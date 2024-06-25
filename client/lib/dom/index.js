b39002dc6ef0914ec90cd99ca44dae8eced4255c
Lee-Jinyong <yjy1480@gmail.com>
2024-06-25 11:38:12 +0900

    주사위 굴리기 완성

--- client/lib/dom/index.js
+++ client/lib/dom/index.js
@@ -4,5 +4,6 @@ export * from './insert.js';
 export * from './getNode.js';
 export * from './showAlert.js';
 export * from './bindEvent.js';
+export * from './endScroll.js';
 export * from './clearContents.js';
 export {default as clearContents} from './clearContents.js'; // default export를 named export처럼 사용할 수 있게 다시 내보내기
