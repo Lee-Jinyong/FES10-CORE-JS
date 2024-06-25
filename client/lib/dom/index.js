d57849797d4975e76509e73c1752b814aafe7de6
Lee-Jinyong <yjy1480@gmail.com>
2024-06-25 10:17:59 +0900

    주접 생성기 완성

--- client/lib/dom/index.js
+++ client/lib/dom/index.js
@@ -2,6 +2,7 @@ export * from './css.js'
 export * from './attr.js';
 export * from './insert.js';
 export * from './getNode.js';
+export * from './showAlert.js';
 export * from './bindEvent.js';
 export * from './clearContents.js';
 export {default as clearContents} from './clearContents.js'; // default export를 named export처럼 사용할 수 있게 다시 내보내기
--- client/main.js
+++ client/main.js
@@ -1,5 +1,16 @@
 import data from './data/data.js';
-import {getNode, getRandom, insertLast, clearContents} from './lib/index.js';
+import {
+  copy,
+  shake,
+  getNode,
+  addClass,
+  showAlert,
+  getRandom,
+  insertLast,
+  removeClass,
+  clearContents,
+  isNumericString,
+} from './lib/index.js';
 
 console.log(data)
 
@@ -10,6 +21,11 @@ console.log(data)
 // 2. input 값 가져오기
 //     - input.value
 // 3. data 함수에서 주접 1개 꺼내기
+// 4. pick 항목 랜더링하기
+
+// [phase-2]
+// 1.  아무 값도 입력 받지 못했을 때 예외처리 (콘솔 출력)
+
 
 const submit = getNode('#submit');
 const nameField = getNode('#nameField');
@@ -22,10 +38,32 @@ function handleSubmit(e) {
   const list = data(name);
   const pick = list[getRandom(list.length)];
 
-  console.log(pick);
+  if(!name || name.replace(/\s*/g, '') === '') {
+    showAlert('.alert-error', '공백은 허용하지 않습니다.');
+    shake('#nameField').restart();
+    return;
+  }
+
+  if(!isNumericString(name)) {
+    showAlert('.alert-error', '제대로된 이름을 입력해 주세요.');
+    shake('#nameField').restart();
+    return;
+  }
 
   clearContents(result);
   insertLast(result, pick);
 }
 
+function handleCopy() {
+  const text = result.textContent;
+  if(nameField.value) {
+    copy(text)
+    .then(() => {
+      showAlert('.alert-success', '클립보드 복사 완료!')
+    })
+  }
+
+}
+
 submit.addEventListener('click', handleSubmit);
+result.addEventListener('click', handleCopy);
