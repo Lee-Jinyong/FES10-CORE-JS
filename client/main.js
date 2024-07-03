b07dbde7c9a5eed66798cd4c9431b3a68c3212e7
Lee-Jinyong <yjy1480@gmail.com>
2024-07-03 13:27:24 +0900

    웹 컴포넌트
    - DOM Style

--- client/main.js
+++ client/main.js
@@ -3,12 +3,18 @@ class UserCard extends HTMLElement {
     super();
     this.attachShadow({mode: 'open'});
     this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          background-color: orange;
+        }
+      </style>
       <div> nickName : kind-tiger </div>
       <slot name="username"></slot>
       <slot name="age"></slot>
       <slot name="gender"></slot>
       <slot name="common"></slot>
       <slot></slot>
+      <slot name="email">email@email.com</slot>
     `
   }
 }
