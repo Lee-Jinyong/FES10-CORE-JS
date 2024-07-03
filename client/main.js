77699ccaf025b929d7afbe3bb6e4e65c75b37bf1
Lee-Jinyong <yjy1480@gmail.com>
2024-07-03 13:45:21 +0900

    웹 컴포넌트
    - event binding

--- client/main.js
+++ client/main.js
@@ -3,20 +3,23 @@ class UserCard extends HTMLElement {
     super();
     this.attachShadow({mode: 'open'});
     this.shadowRoot.innerHTML = `
-      <style>
-        :host {
-          background-color: orange;
-        }
-      </style>
-      <div> nickName : kind-tiger </div>
-      <slot name="username"></slot>
-      <slot name="age"></slot>
-      <slot name="gender"></slot>
-      <slot name="common"></slot>
-      <slot></slot>
-      <slot name="email">email@email.com</slot>
+      <button type="button">btn</button>
     `
+
+    this.button = this.shadowRoot.querySelector('button')
+
+    console.log(this.button)
+  }
+
+  connectedCallback() {
+    this.button.addEventListener('click', this.clickMe.bind(this));
+  }
+
+  clickMe() {
+    console.log(this);
   }
 }
 
 customElements.define('user-card', UserCard);
+
+// console.log(document.querySelector('.user-card').shadowRoot.querySelector('button'))
