2b23d054344b9b5897127365cb586993ab1ccff8
Lee-Jinyong <yjy1480@gmail.com>
2024-07-03 11:15:14 +0900

    웹컴포넌트
    - shadow DOM

--- client/main.js
+++ client/main.js
@@ -1,6 +1,14 @@
-class Button extends HTMLButtonElement {
+class Button extends HTMLElement {
   constructor() {
     super();
+    
+    // c-button의 쉐도우 돔을 열기
+    const shadow = this.attachShadow({mode:'open'}); // open, closed
+
+    // 쉐도우 돔 내부에 내가 원하는 태그 넣기
+    shadow.innerHTML = `
+    <button>hello</button>
+    `
   }
 
   connectedCallback() {
@@ -13,4 +21,6 @@ class Button extends HTMLButtonElement {
 
 }
 
-customElements.define('c-button', Button, {extends: 'button'});
+customElements.define('c-button', Button);
+
+console.log(document.querySelector('c-button').shadowRoot.querySelector('button'));
